add30d1ab83919b4ba78462a4504bac19effba87
Elisa <elisa.torazza@gmail.com>
2020-09-17 12:06:25 +0200

    Push

--- js/script.js
+++ js/script.js
@@ -2,22 +2,24 @@ $(document).ready(function() {
 
 $(".genera").click(function(){
   var selectedButton = $(this);
-  $.ajax(
- {
- "url": "https://flynn.boolean.careers/exercises/api/random/int",
- "method": "GET",
- "success": function (data, stato) {
-   $(".genera").addClass("none");
 
-   var numero = data["response"];
-   selectedButton.next().text(numero);
-   if (numero <= 5){
-     selectedButton.parents(".box").addClass("yellow");
-   } else {
-     selectedButton.parents(".box").addClass("green");
-   }
- },
- "error": function (richiesta, stato, errori) {
+
+    $.ajax(
+   {
+   "url": "https://flynn.boolean.careers/exercises/api/random/int",
+   "method": "GET",
+   "success": function (data, stato) {
+     selectedButton.addClass("none");
+
+     var numero = data["response"];
+     selectedButton.next().text(numero);
+     if (numero <= 5){
+       selectedButton.parents(".box").addClass("yellow");
+     } else {
+       selectedButton.parents(".box").addClass("green");
+     }
+   },
+   "error": function (richiesta, stato, errori) {
 
  }
  }
